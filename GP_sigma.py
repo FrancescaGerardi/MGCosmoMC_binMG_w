3cbf0907c2d95f0738527d9fa84a44ed37c3696b
FrancescaGerardi <francesca.gerardi@hotmail.it>
2018-07-23 19:28:40 +0200

    params_binnedMG

--- GP_sigma.py
+++ GP_sigma.py
@@ -16,9 +16,9 @@ parser.add_argument('--ODEsteps',metavar='ODEsteps',  type=int, nargs='+',
                    help='number of steps for the ODE solver')
 parser.add_argument('--scalefactors',metavar='a',  type=float, nargs='*',default=[],
                    help='values of redshifts')
-parser.add_argument('--eos',metavar='wde',  type=float, nargs='*',default=[],
+parser.add_argument('--sigma',metavar='sigma',  type=float, nargs='*',default=[],
                    help='equation of state')
-parser.add_argument('--eosn',metavar='wn',  type=float, nargs='*',default=[],
+parser.add_argument('--sigman',metavar='sn',  type=float, nargs='*',default=[],
                    help='equation of state at z=0')
 parser.add_argument('--l',metavar='l',  type=float, nargs='+',
                    help='correlation length')
@@ -38,62 +38,73 @@ inia = args.inia[0]
 enda = args.enda[0]
 ODEsteps = args.ODEsteps[0]
 #z_edge = np.array(args.redshifts) #NH redshift at edge of each bin
-wde = np.array(args.eos)
-
-nb=len(wde)
-
-wn = np.array(args.eosn)
+sig = np.array(args.sigma)
+sn = np.array(args.sigman)
 l = args.l[0]
 filename = args.outfile[0]
 
 a = np.array(args.scalefactors)#z_edge[:-1] + np.diff(z_edge)/2 #NH z is now the redshift in the middle of each bin
 ini=[inia]
+a_d=np.concatenate([ini,a])
+#print  a
+sig_d=np.concatenate([sn,sig])
+#print  sig
 
-a=np.concatenate([ini,a])
-wde=np.concatenate([wn,wde])
-
-#GP smoothed at last point
-sma=[a[nb]-0.01]
-a_gp=np.concatenate([a,sma])
-smw=[wde[nb]]
-wde_gp=np.concatenate([wde,smw])
+nb=len(sig)
+#print nb
+a=np.zeros(nb+1)
+sig=np.zeros(nb+1)
+#RIORDINO IN MANIERA CRESCENTE --> redshift decrescente
+for i in range (nb+1):
+    #print i
+    a[i] = a_d[nb-i]
+    sig[i] = sig_d[nb-i]
+#print a
+#print sig
 
 
 #defining the baseline -1
-base = lambda x: -1+x-x
+base = lambda x: +1+x-x
 
 # Generation of the Gaussian Process
 gp = GaussianProcessRegressor(kernel=RBF(l, (l, l)))
 
 #Fit --> Training
-g = gp.fit(a_gp[:, np.newaxis], wde_gp-base(a_gp))
+g = gp.fit(a[:, np.newaxis], sig-base(a))
 
 #Plotting points (if log use np.logspace)
-a_sampling = np.linspace(inia, enda, ODEsteps)
+a_sampling = np.linspace(enda, inia, ODEsteps)
 #print a_sampling
 
 #transforming a_sampling in z_sampling
 z_sampling=np.zeros(ODEsteps)
+a_rev=np.zeros(ODEsteps)
 for i in range (ODEsteps):
-    z_sampling[i]= -1 + 1/a_sampling[i]
+    z_sampling[i]= -1 + 1/a_sampling[ODEsteps-1-i]
+    a_rev[i]=a_sampling[ODEsteps-1-i]
 #print z_sampling
 #Predict points
-w_pred, sigma = gp.predict(a_sampling[:, np.newaxis], return_std=True)
-w_pred = w_pred + base(a_sampling)
+sig_pred_a, sigma = gp.predict(a_sampling[:, np.newaxis], return_std=True)
+sig_pred_a = sig_pred_a + base(a_sampling)
+
+sig_pred=np.zeros(ODEsteps)
+for i in range (ODEsteps):
+    sig_pred[i]= sig_pred_a[ODEsteps-1-i]
+#print sig_pred
 
 #Plot the result: remove it from final verions
 #fig= plt.figure(figsize=(14,12))
-#plt.plot(a_sampling, w_pred, label = 'l=%s'%l)
+#plt.plot(a_sampling, sig_pred_a, label = 'l=%s'%l)
 #plt.legend(fontsize=20)
-#plt.scatter(a, wde)
-#fig.savefig('test_figure.png')
+#plt.scatter(a, sig)
+#fig.savefig('test_figure_sigma_a.png')
 
 # print to file
-#f = open(filename,'w')
+#f = open(filename,'mu')
 # print len(z_sampling)
 #for i in range(0, ODEsteps):
-#    print >>f, z_sampling[i], w_pred[i]
-np.savetxt(filename, np.array([z_sampling, w_pred]).T, fmt="%15.8e")
+#    print >>f, z_sampling[i], mu_pred[i]
+np.savetxt(filename, np.array([a_rev,z_sampling, sig_pred]).T, fmt="%15.8e")
 
 #f.close()
 
