8fbca6f0b79a198c3d4c5c35ee8c2745ab9a7151
FrancescaGerardi <38529640+FrancescaGerardi@users.noreply.github.com>
2018-08-07 11:35:46 +0200

    Update binnedMG.f90

--- camb/binnedMG.f90
+++ camb/binnedMG.f90
@@ -4,7 +4,7 @@ use constants
 use ModelParams
 
       implicit none
-      logical, parameter :: debugging = .true.
+      logical, parameter :: debugging = .false.
       real(dl), dimension(:),allocatable :: derivm, derivs, derivg
       real(dl), dimension(:),allocatable :: binned_z, binned_mu, binned_sigma  !output arrays of GP reconstruction
       real(dl), dimension(:),allocatable :: g1, c1, d1                         !coefficients for interpolation
